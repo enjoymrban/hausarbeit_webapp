b6423efa0f4c658f1bee1dc5e75af009fbc58427
Silvan Knecht <knecht.silvan@gmail.com>
2018-04-29 11:13:51 +0200

    css added4

--- hausarbeit_webapp/public/javascripts/main.js
+++ hausarbeit_webapp/public/javascripts/main.js
@@ -30,11 +30,13 @@ function learnnow(context) {
             .appendTo(context.$element())
             .then(function () {
                 $.each(json, function(key, value) {
-                    var card = $('<div class="col-md-4"><div class="card"><div class="card-body"><h4 class="card-title">'+value.title+'</h4><p class="card-text">'+value.description+'</p></div></div>')
+
+                    var card = $('<div class="col-xl-3 col-md-4 col-sm-6 d-flex align-items-stretch"><div class="card"><div class="card-body"><h4 class="card-title">'+value.title+'</h4><p class="card-text">'+value.description+'</p><div class="custom-card-footer"><img src="/assets//images/icons/settings.svg" alt="addBox"  width="35" height="35" /></div></div></div>')
                     $(".box" ).append(card);
 
+
                 });
-                var createCard = $('<div class="col-md-4"><div class="card"><div class="card-body"><img src="@routes.Assets.at("/images/test1.png")" width="28" height="22" /></div></div>')
+                var createCard = $('<div class="col-xl-3 col-md-4 col-sm-6 d-flex align-items-stretch"><div class="card align-items-center"><div class="card-body "><img src="/assets//images/icons/add.svg" alt="addBox"  width="80" height="80" /></div></div></div>')
                 $(".box" ).append(createCard);
 
             });
