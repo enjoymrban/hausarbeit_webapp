13e5f1be4b9a2fd479c4f205994a1129292d5efc
Silvan Knecht <knecht.silvan@gmail.com>
2018-04-29 09:09:25 +0200

    edited all front end

--- hausarbeit_webapp/public/javascripts/main.js
+++ hausarbeit_webapp/public/javascripts/main.js
@@ -1,14 +1,42 @@
-var app = $.sammy('#app', function() { // definiert neue Sammy application und bindet diese an #app
-
-    this.get('#/books', function(context) { // neue route
-        context.app.swap('boxes.html'); // ersetzt den Inhalt vom app Element mit ''
+var app = $.sammy('#app', function() {
+    this.get('#/learnnow', function(context) {
+        context.app.swap('');
+        learnnow(context);
+    });
+    /*this.get('#/books', function(context) {
+        context.app.swap('');
         books(context);
     });
-
-
-    this.get('/#/bestseller', function(context) {
+    this.get('#/bestseller', function(context) {
         context.app.swap('');
-        context.render('/assets/html/bestseller.html', {}) // lädt die HTML Datei und fügt den Inhalt dem #app Element an
-            .appendTo(context.$element());
-    });
+        bestseller(context);
+    });*/
 });
+
+
+jQuery(function() {
+    app.run('#/learnnow');
+});
+
+
+function learnnow(context) {
+    var url = '/api/box';
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType : "json"
+    }).done(function(json)  {
+        context.render('/assets/html/learnnow.html', {})
+            .appendTo(context.$element())
+            .then(function () {
+                $.each(json, function(key, value) {
+                    var card = $('<div class="col-md-4"><div class="card"><div class="card-body"><h4 class="card-title">'+value.title+'</h4><p class="card-text">'+value.description+'</p></div></div>')
+                    $(".box" ).append(card);
+
+                });
+                var createCard = $('<div class="col-md-4"><div class="card"><div class="card-body"><img src="@routes.Assets.at("/images/test1.png")" width="28" height="22" /></div></div>')
+                $(".box" ).append(createCard);
+
+            });
+    });
+}
